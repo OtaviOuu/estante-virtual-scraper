f22d4539f41ffeb08e4251007606fb20007c3da5
OtaviOuu <conta.otaviou@gmail.com>
2025-01-21 19:00:52 -0300

    Implement SQLite database integration for book data storage in EstanteVirtual spider

--- src/scrape.py
+++ src/scrape.py
@@ -4,8 +4,8 @@ from scrapy.http import Response
 
 from random_user_agent.user_agent import UserAgent
 from slugify import slugify
+import sqlite3
 import json
-
 
 user_agent_rotator = UserAgent()
 
@@ -13,6 +13,35 @@ user_agent_rotator = UserAgent()
 class EstanteVirtual(Spider):
     name = "estante_virtual"
     base_url = f"https://www.estantevirtual.com.br"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.conn = sqlite3.connect("estante_virtual.db")
+        self.cursor = self.conn.cursor()
+        self._create_table()
+
+    def _create_table(self):
+        self.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS books (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                book_name TEXT,
+                author TEXT,
+                book_id TEXT,
+                book_group_id TEXT,
+                description TEXT,
+                is_group BOOLEAN,
+                type TEXT,
+                list_price REAL,
+                sale_price REAL,
+                image TEXT,
+                link TEXT,
+                attributes TEXT,
+                category TEXT
+            )
+            """
+        )
+        self.conn.commit()
 
     def start_requests(self):
         self.base_header = {
@@ -32,26 +61,48 @@ class EstanteVirtual(Spider):
         ).getall()
 
         for category in categorys:
-            for page_index in range(1, 5):
-                url = f"{self.base_url}{category}?tipo-de-livro=usado&page={page_index}"
-                yield Request(
-                    url=url,
-                    headers={
-                        "cookie": "for some reason this is needed",
-                        "user-agent": user_agent_rotator.get_random_user_agent(),
-                    },
-                    callback=self.get_books,
-                )
+            # https://www.estantevirtual.com.br/medicina?page=289&tipo-de-livro=usado
+            url = f"{self.base_url}{category}?tipo-de-livro=usado"
+
+            yield Request(
+                url=url,
+                headers=self.base_header,
+                callback=self.get_max_pagination,
+            )
+            """for page_index in range(1, 30):
+            url = f"{self.base_url}{category}?tipo-de-livro=usado&page={page_index}"
+            yield Request(
+                url=url,
+                headers={
+                    "cookie": "for some reason this is needed",
+                    "user-agent": user_agent_rotator.get_random_user_agent(),
+                },
+                callback=self.get_books,
+            )
+            """
+
+    def get_max_pagination(self, response: Response):
+        last_page = response.css(".pagination__page::text").getall()[-1].strip()
+
+        with open("last_page.txt", "a") as f:
+            f.write(f"{response.url} - {last_page}\n")
+
+        for page_index in range(1, int(last_page) + 1):
+            url = f"{response.url}&page={page_index}"
+            yield Request(
+                url=url,
+                headers={
+                    "cookie": "for some reason this is needed",
+                    "user-agent": user_agent_rotator.get_random_user_agent(),
+                },
+                callback=self.get_books,
+            )
 
     def get_books(self, response: Response):
         try:
-            # Horrivel, mas ta bom por agr
             data_layer = json.loads(
                 response.css("script::text")[1].get().strip().split("= ")[1]
             )[0]
-
-            with open("./logs/1_get_books.json", "w") as f:
-                json.dump(data_layer, f, ensure_ascii=False, indent=4)
 
             books = data_layer["ecommerce"]["impressions"]
             for book in books:
@@ -85,17 +136,12 @@ class EstanteVirtual(Spider):
                 response.css("script")[-3]
                 .get()
                 .strip()
-                # ruim mas funciona
                 .replace("<script>window.__INITIAL_STATE__=", "")
                 .replace("</script>", "")
             )
 
             data_json = json.loads(data_layer)
 
-            """ 
-            with open("2_get_book_data.json", "w") as f:
-                json.dump(data_json, f, ensure_ascii=False, indent=4)
-            """
             formated_atributes = data_json["Product"]["formattedAttributes"]
             author = data_json["Product"]["author"]
 
@@ -128,12 +174,6 @@ class EstanteVirtual(Spider):
     def get_grup_book_data(self, response: Response):
         try:
             data = response.json()
-            books_list = []
-
-            """
-            with open("3_get_group_book_data.json", "w") as f:
-                json.dump(data, f, ensure_ascii=False, indent=4)
-            """
 
             aggregates = data["aggregates"]
 
@@ -144,7 +184,7 @@ class EstanteVirtual(Spider):
             skus = data["parentSkus"]
             for sku in skus:
                 book_unit = {
-                    "name": sku["name"],
+                    "book_name": response.meta["name"],
                     "author": response.meta["author"],
                     "book_id": sku["productCode"],
                     "book_group_id": sku["itemGroupId"],
@@ -155,32 +195,56 @@ class EstanteVirtual(Spider):
                     "sale_price": sku["salePrice"],
                     "image": sku["image"],
                     "link": f"{self.base_url}/livro/{slugify(sku['name'])}-{sku['productCode']}",
-                    "attributes": response.meta["formatted_atributes"],
-                    "category": category,
+                    "attributes": json.dumps(response.meta["formatted_atributes"]),
+                    "category": json.dumps(category),
                 }
-                books_list.append(book_unit)
 
-            book_data = {
-                "book_name": response.meta["name"],
-                "books_list": books_list,
-            }
-
-            yield {
-                "book_name": response.meta["name"],
-            }
+                # Inserindo no SQLite
+                self.cursor.execute(
+                    """
+                    INSERT INTO books (
+                        book_name, author, book_id, book_group_id, description,
+                        is_group, type, list_price, sale_price, image, link,
+                        attributes, category
+                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    """,
+                    (
+                        book_unit["book_name"],
+                        book_unit["author"],
+                        book_unit["book_id"],
+                        book_unit["book_group_id"],
+                        book_unit["description"],
+                        book_unit["is_group"],
+                        book_unit["type"],
+                        book_unit["list_price"],
+                        book_unit["sale_price"],
+                        book_unit["image"],
+                        book_unit["link"],
+                        book_unit["attributes"],
+                        book_unit["category"],
+                    ),
+                )
+                self.conn.commit()
 
         except (IndexError, json.JSONDecodeError, KeyError) as e:
             print(f"Error parsing group book data: {e}")
+
+    def __del__(self):
+        self.conn.close()
 
 
 process = CrawlerProcess(
     settings={
         "FEEDS": {
-            "books.json": {"format": "json"},
+            "books.json": {
+                "format": "json",
+                "encoding": "utf8",
+                "indent": 4,
+                "ensure_ascii": False,
+            },
         },
     }
 )
-
 
 process.crawl(EstanteVirtual)
 process.start()
